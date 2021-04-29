7e50bda8b399f0e7361fbe9cd9419369b1901cc5
Roman Abdoullaev <romanabdoullaev@gmail.com>
2021-04-29 14:11:19 -0600

    Added 002 file. Which is the insertion sort algorighm that sorts in descending order.

--- 002_InsertionSort_descending.py
+++ 002_InsertionSort_descending.py
@@ -1,3 +1,4 @@
+#this algorithm sorts the input array in descending order
 print('Running...')
 
 A = [5,2,4,6,1,3]
@@ -7,7 +8,7 @@ for j in range(1,len(A)): #iterate over the A array starting with second element
     #Move elements of A[0..j], that are greater than key, to one position ahead
     #of their current position.
     i=j-1
-    while i>=0 and A[i]>key:
+    while i>=0 and A[i]<key:
         A[i+1]=A[i]
         i=i-1
     A[i+1]=key
