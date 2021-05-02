d46c16055d396d1aae3a12891114095ff971e0c4
Roman Abdoullaev <romanabdoullaev@gmail.com>
2021-05-01 22:36:17 -0600

    Initial commit.

---
+++ __Template.py
@@ -0,0 +1,16 @@
+#Python program for performing...
+
+#Function for..
+#Input:
+#Output:
+#Limitations:
+def merge(A,p):
+
+    return A #return the sorted array A
+
+
+#Driver code to test above 
+A=[1,2,3]
+
+#Call and print the results of...
+print(merge(A, len(A)))
