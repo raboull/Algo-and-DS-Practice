485497f6c94ea2dc1cd9a5c893534b97adb18710
Roman Abdoullaev <romanabdoullaev@gmail.com>
2021-05-03 14:28:51 -0600

    Updated to terminate search when first matching element is encountered.

--- 003_LinearSearch.py
+++ 003_LinearSearch.py
@@ -5,16 +5,16 @@
 #Limitation: looks through every element and returns the index of last 
 #element that contains value v.
 def LinearSearch_1(arr, v):
-    match = None
+    
     for i in range(0,len(arr)): #iterate through every element of arr
         if arr[i]==v:#check if current element is equal to v
-            match = i+1
+            return i
     
-    return match
+    return None
 
 #Driver code to test above
-A = [5,2,4,6,1,3]
-v = 5
+A = [5,2,4,6,1,1]
+v = 1
 result = LinearSearch_1(A,v)
 
 if result== None:
