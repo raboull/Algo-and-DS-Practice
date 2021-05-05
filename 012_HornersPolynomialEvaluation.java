f823b5e0f3e936732840e9da2385f3cd7f90e4ed
Roman Abdoullaev <romanabdoullaev@gmail.com>
2021-05-04 18:39:40 -0600

    Updated for loop index range in the hornersPolynomialEvaluation method.

--- 012_HornersPolynomialEvaluation.java
+++ 012_HornersPolynomialEvaluation.java
@@ -7,10 +7,10 @@ class HornersPolynomialEvaluation
     //      A - array of constant coeffiecients in the polynomail equation that correspond to exponentiated x in increasing order.
     //      x - value at which we would like to evaluate our exponential equation.
     //Output: evaluated polynomial equation answer as an integer.
-    public int naivePolynomialEvaluation(int A[], int x)
+    public int hornersPolynomialEvaluation(int A[], int x)
     {
         int y = 0;//intialize the answer to 0
-        for (int i=A.length; i>=0; i--){
+        for (int i=A.length-1; i>=0; i--){
             y = A[i]+x*y;
         }
         return y;
@@ -22,10 +22,10 @@ class HornersPolynomialEvaluation
         int TestArray[] = {-1, 2, -6, 2};//initialize an array that will be used for testing
         int x = 3;
 
-        NaivePolynomialEvaluation object = new NaivePolynomialEvaluation();
+        HornersPolynomialEvaluation object = new HornersPolynomialEvaluation();
         
         //test the bubblesort method
-        System.out.println(object.naivePolynomialEvaluation(TestArray, x));
+        System.out.println(object.hornersPolynomialEvaluation(TestArray, x));
     }    
 }
 
